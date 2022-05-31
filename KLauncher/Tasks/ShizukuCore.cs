fe912524be442004c36c590f91ad5c2f1e03baaf
dinghaoxiang <michael19950415@outlook.com>
2022-05-31 19:37:27 +0800

    shizuku add

---
+++ KLauncher/Tasks/ShizukuCore.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Rikka.Shizuku;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KLauncher.Tasks
+{
+    public sealed class ShizukuCore
+    {
+        public object S
+        {
+            get
+            {
+                var wrapper = new ShizukuBinderWrapper(SystemServiceHelper.GetSystemService("package"));
+                return wrapper;
+            }
+        }
+    }
+}
